e0100ae5d1b193411f5aa1bd9f0c30f49fb7b67a
Deepa Kiwne <deepa.kiwne@gmail.com>
2019-10-01 22:49:44 -0700

    fix: Correct shelf in selected in MyReads page

--- src/Book.js
+++ src/Book.js
@@ -13,7 +13,7 @@ class Book extends React.Component {
               <div className="book-cover" style={{ width: 128, height: 193, 
                   backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
               <div className="book-shelf-changer">
-                <select>
+                <select value={book.shelf}>
                   <option value="move" disabled>Move to...</option>
                   <option value="currentlyReading">Currently Reading</option>
                   <option value="wantToRead">Want to Read</option>
